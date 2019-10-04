4788eca878d748b1ba590db27ef45324bd621569
Lucas Dantas <luk.dantas@gmail.com>
2019-10-04 00:00:32 -0300

    Estado e cidade com repositorios

---
+++ src/main/java/com/lluke8/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.lluke8.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lluke8.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
