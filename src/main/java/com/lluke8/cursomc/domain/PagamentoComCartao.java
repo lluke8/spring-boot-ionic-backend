bfd04b09b9785706572e0b6fc3ac5bb6e1d7711c
Lucas Dantas <luk.dantas@gmail.com>
2019-10-07 19:57:53 -0300

    Pedido, estadopedido, pagamento , parte2

--- src/main/java/com/lluke8/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/lluke8/cursomc/domain/PagamentoComCartao.java
@@ -1,5 +1,26 @@
 package com.lluke8.cursomc.domain;
 
+import com.lluke8.cursomc.domain.enums.EstadoPagamento;
+
 public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
 	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
 }
