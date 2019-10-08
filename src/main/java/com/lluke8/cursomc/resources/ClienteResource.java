96d717fa03a9626ded6b42858e8f0da64693c95e
Lucas Dantas <luk.dantas@gmail.com>
2019-10-08 20:55:09 -0300

    PUT, DELETE, e GET para Cliente

--- src/main/java/com/lluke8/cursomc/resources/ClienteResource.java
+++ src/main/java/com/lluke8/cursomc/resources/ClienteResource.java
@@ -1,16 +1,22 @@
 package com.lluke8.cursomc.resources;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lluke8.cursomc.domain.Cliente;
+import com.lluke8.cursomc.dto.ClienteDTO;
 import com.lluke8.cursomc.services.ClienteService;
 import com.lluke8.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -26,9 +32,45 @@ public class ClienteResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		
-		
 		Cliente obj = service.find(id);
-		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() throws ObjectNotFoundException {
+		
+		List<Cliente> list = service.findAll(); 
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue = "ASC") String direction) 
+					throws ObjectNotFoundException {
+		
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction); 
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
