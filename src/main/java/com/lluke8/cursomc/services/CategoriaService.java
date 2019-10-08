2796d499286975365c7a4109d223870ab5c9dd2c
Lucas Dantas <luk.dantas@gmail.com>
2019-10-08 19:07:24 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/lluke8/cursomc/services/CategoriaService.java
+++ src/main/java/com/lluke8/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lluke8.cursomc.domain.Categoria;
+import com.lluke8.cursomc.dto.CategoriaDTO;
 import com.lluke8.cursomc.repositories.CategoriaRepository;
 import com.lluke8.cursomc.services.exceptions.DataIntegrityException;
 import com.lluke8.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,8 +54,14 @@ public class CategoriaService {
 	}
 	
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		//PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
