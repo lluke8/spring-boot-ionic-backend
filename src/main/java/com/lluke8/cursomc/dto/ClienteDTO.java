1fbb2a604ff9b64eee858c48b17d003237409aef
Lucas Dantas <luk.dantas@gmail.com>
2019-10-11 14:43:38 -0300

    Validação customizada: Email não repetido na atualização de Cliente

--- src/main/java/com/lluke8/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/lluke8/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.lluke8.cursomc.domain.Cliente;
+import com.lluke8.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
